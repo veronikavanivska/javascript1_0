0b15cfdd745aad79c86336a56f3c040552808e11
veronikavanivska <veronikavanivska2005@gmail.com>
2024-10-03 11:57:41 +0200

    First task

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const btn = document.querySelector("#ex1_button");
 (function () {
-  //TODO
+    btn.onclick = function() {
+        const content = document.querySelector("#ex1_content");
+        content.textContent = Array.from({length: 10}, (_, i) => i).join(', ');
+    }
 })();
