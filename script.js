25a15ba5b7bcd6e9425ed205db05ac609b20f29c
veronikavanivska <veronikavanivska2005@gmail.com>
2024-10-03 12:19:03 +0200

    Second task

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ const btn = document.querySelector("#ex1_button");
         content.textContent = Array.from({length: 10}, (_, i) => i).join(', ');
     }
 })();
+
+const inputField = document.querySelector("#ex2_text");
+const content = document.querySelector("#ex2_content");
+
+inputField.addEventListener('input', function() {
+    const value = inputField.value;
+
+    if (/[a-zA-Z]/.test(value)) {
+        content.textContent = "The number cannot contain letters";
+    }
+    else if (/[^0-9]/.test(value)) {
+        content.textContent = "The number cannot contain special characters";
+    }
+    else if (value.length !== 9) {
+        content.textContent = "Length of the number have to be equal 9";
+    }
+    else {
+        content.textContent = "The number is correct";
+    }
+});
+
